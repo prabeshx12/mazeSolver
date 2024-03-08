7e6d85d071583f083a5f9b0de16c6efec0ff91ed
Prabesh Bashyal <bashyalprabesh.12@gmail.com>
2024-03-08 13:56:49 +0545

    test_maze_solver

---
+++ introWindow.py
@@ -0,0 +1,72 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import new_maze_solver
+
+root = Tk()
+root.geometry('1000x800')
+root.title("Maze Solver")
+root.maxsize(height=800, width=1000)
+root.minsize(height=800, width=1000)
+root.config(bg="lightseagreen")
+
+
+def solve_maze():
+    value = int(r.get())
+    new_maze_solver.start_solving(value)
+
+
+def free_turtle():
+    new_maze_solver.free()
+
+
+r = IntVar()
+
+# for background image
+background_image = Image.open("images/background.png")
+resized_image = background_image.resize((350, 400))
+res_background_image = ImageTk.PhotoImage(resized_image)
+
+title = Label(root, text="We present to you Maze Solver", font="TimesNewRoman, 20", bg="lightblue", height=2, width=50)
+title.pack(pady=20)
+
+image_frame = Frame(root)
+image_frame.pack(pady=20)
+
+image_maze = Label(image_frame, height=350, width=400, image=res_background_image)
+image_maze.pack(pady=20)
+
+select_level = Label(root, text="Please chose your maze difficulty level!!", font="TimesNewRoman, 18", bg="lightpink",
+                     height=1, width=50)
+select_level.pack(pady=20)
+
+# background for radio buttons
+background_radio = Frame(root, bg="lightseagreen")
+background_radio.pack()
+
+# radio easy
+easy_radio = Radiobutton(background_radio, variable=r, value=0, text="EASY", font="TimesNewRoman, 15")
+easy_radio.pack(padx=25, pady=10, side=LEFT)
+
+# radio medium
+medium_radio = Radiobutton(background_radio, variable=r, value=1, text="MEDIUM", font="TimesNewRoman, 15")
+medium_radio.pack(padx=25, pady=10, side=LEFT)
+
+# radio hard
+hard_radio = Radiobutton(background_radio, variable=r, value=2, text="HARD", font="TimesNewRoman, 15")
+hard_radio.pack(padx=25, pady=10, side=LEFT)
+
+# radio impossible
+impossible_radio = Radiobutton(background_radio, variable=r, value=3, text="IMPOSSIBLE", font="TimesNewRoman, 15")
+impossible_radio.pack(padx=25, pady=10, side=RIGHT)
+
+button_frame = Frame(root, bg="lightseagreen")
+button_frame.pack(pady=20)
+
+solve_button = Button(button_frame, text="CREATE AND SOLVE", font="TimesNewRoman, 15", command=solve_maze)
+solve_button.pack(padx=50, side=LEFT)
+
+free_turtle_button = Button(button_frame, text="FREE TURTLE", font="TimesNewRoman, 15", command=free_turtle)
+free_turtle_button.pack(side=RIGHT)
+
+if __name__ == "__main__":
+    root.mainloop()
---
+++ new_maze_solver.py
@@ -0,0 +1,295 @@
+import turtle
+import time
+import sys
+from collections import deque
+from tkinter import messagebox
+
+
+class Maze(turtle.Turtle):  # define a Maze class
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("brown")
+        self.penup()
+        self.speed(0)
+
+
+class Green(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("green")
+        self.penup()
+        self.speed(0)
+
+
+class Blue(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("blue")
+        self.penup()
+        self.speed(0)
+
+
+
+class Yellow(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("yellow")
+        self.penup()
+        self.speed(0)
+
+
+class White(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.speed(0)
+
+
+grids = [
+    [
+        "+++++++++++++++++++++++++++++++++++++++++++++++++++",  # simple maze
+        "e                                                 +",
+        "+                                                 +",
+        "+                                                 +",
+        "+                                                 +",
+        "+                                                 +",
+        "+++++++++++++++++++++++++++++++++++++++++++++++++ +",
+        "+                                                 +",
+        "+ +++++++++++++++++++++++++++++++++++++++++++++++++",
+        "+                                                 +",
+        "+++++++++++++++++++++++++++++++++++++++++++++++++ +",
+        "+                                                 +",
+        "+ +++++++++++++++++++++++++++++++++++++++++++++++++",
+        "+                                                 +",
+        "+++++++++++++++++++++++++++++++++++++++++++++++++ +",
+        "+                                                 +",
+        "+ +++++++++++++++++++++++++++++++++++++++++++++++++",
+        "+                                                 +",
+        "+++++++++++++++++++++++++++++++++++++++++++++++++ +",
+        "+                                                 +",
+        "+ +++++++++++++++++++++++++++++++++++++++++++++++++",
+        "+                                                 +",
+        "+++++++++++++++++++++++++++++++++++++++++++++++++ +",
+        "s                                                 +",
+        "+++++++++++++++++++++++++++++++++++++++++++++++++++",
+        ],
+    
+    [
+        "+++++++++++++++++++++++++++++++++++++++++++++++++++",  # medium maze
+        "+               +                                 s",
+        "+  ++++++++++  +++++++++++++  +++++++  ++++++++++++",
+        "+           +                 +               ++  +",
+        "+  +++++++  +++++++++++++  +++++++++++++++++++++  +",
+        "+  +     +  +           +  +                 +++  +",
+        "+  +  +  +  +  +  ++++  +  +  +++++++++++++  +++  +",
+        "+  +  +  +  +  +  +        +  +  +        +       +",
+        "+  +  ++++  +  ++++++++++  +  +  ++++  +  +  ++   +",
+        "+  +     +  +          +   +           +  +  ++  ++",
+        "+  ++++  +  +++++++ ++++++++  +++++++++++++  ++  ++",
+        "+     +  +     +              +              ++   +",
+        "++++  +  ++++++++++ +++++++++++  ++++++++++  +++  +",
+        "+  +  +                    +     +     +  +  +++  +",
+        "+  +  ++++  +++++++++++++  +  ++++  +  +  +  ++   +",
+        "+  +  +     +     +     +  +  +     +     +  ++  ++",
+        "+  +  +  +++++++  ++++  +  +  +  ++++++++++  ++  ++",
+        "+                       +  +  +              ++  ++",
+        "+ ++++++             +  +  +  +  +++        +++  ++",
+        "+ ++++++ ++++++ +++++++++    ++ ++   ++++++++++  ++",
+        "+ +    +    +++ +     +++++++++ ++  +++++++    + ++",
+        "+ ++++ ++++ +++ + +++ +++    ++    ++    ++ ++ + ++",
+        "+ ++++    +     + +++ +++ ++ ++++++++ ++ ++ ++   ++",
+        "+      ++ +++++++ +++     ++          ++    +++++++",
+        "+++++++++++++++++e+++++++++++++++++++++++++++++++++",
+            ],
+
+
+    [
+        "+++++++++++++++++++++++++++++++++++++++++++++++++++",  # hard maze
+        "s  +                      +      ++            ++ +",
+        "+  ++++   +++++++   +++++ ++++   +   +++++        +",
+        "+          +      ++++    +         + +   +    ++ +",
+        "+  ++++ +++++ ++++++++ ++++++  +++++++++++++ ++++ +",
+        "+  +   +     + + +       +   + +     +       +  + +",
+        "+  ++++ +++++ +  +  +++ + + +  ++++ ++ +++++++ +  +",
+        "+       +    +    +  +   + + +  +  +   +          +",
+        "+  ++++ +++++ ++++  +++++++++ +  +++ ++++++ ++++ ++",
+        "+     +    +      +        +        +   +    + +  e",
+        "+++++ + ++++++++ + +++++++++ ++++++ ++++ + ++++   +",
+        "+   + +         + +          + +       + +      + +",
+        "+++++ + ++++++ +++ + +++++++ + ++++ +++ ++ +++++ ++",
+        "+     +       + +            ++          +     +  +",
+        "+ ++++++++++ ++++++++++ ++ +++++++ ++++ + ++++ ++++",
+        "+          + +    +       +     + +   + +        ++",
+        "++++++ ++++++++++ + ++++ +++++++++ + ++  ++  ++++ +",
+        "+       +       + +    + +     +   + + + + +    + +",
+        "+  ++++++ ++++  ++++ + ++ ++ + + + + +++ + +++ + ++",
+        "+  +  +   +   + +  +  +   +     + +   +   +     + +",
+        "+  +  +++  + +   ++     + + + + + +++    +++ +  + +",
+        "+         + + + +   ++ +    +  ++  +++++        + +",
+        "+  +++++++++ ++  + +++++ ++    + ++++ ++++ ++++++++",
+        "+  +   +    +             + +  +                  +",
+        "+++++++++++++++++++++++++++++++++++++++++++++++++++",
+
+        ],
+
+    [
+        "+++++++++++++++++++++++++++++++++++++++++++++++++++",  # impossible maze
+        "s  +                      +      ++            ++ +",
+        "+  ++++   +++++++   +++++ ++++   +   +++++        +",
+        "+          +      ++++    +         + +   +    ++ +",
+        "+  ++++ +++++ ++++++++ ++++++  +++++++++++++ ++++ +",
+        "+  +   +     + + +       +   + +e"
+        "    +       +  + +",
+        "+  ++++ +++++ +  +  +++ + + +  ++++ ++ +++++++ +  +",
+        "+       +    +    +  +   + + +  +  +   +          +",
+        "+  ++++ +++++ ++++  +++++++++ +  +++ ++++++ ++++ ++",
+        "+     +    +      +        +        +   +    + +  +",
+        "+++++ + ++++++++ + +++++++++ ++++++ ++++ + ++++   +",
+        "+   + +         + +          + +       + +      + +",
+        "+++++ + ++++++ +++ + +++++++ + ++++ +++ ++ +++++ ++",
+        "+     +       + +            ++          +     +  +",
+        "+ ++++++++++ ++++++++++ ++ +++++++ ++++ + ++++ ++++",
+        "+          + +    +       +     + +   + +        ++",
+        "++++++ ++++++++++ + ++++ +++++++++ + ++  ++  ++++ +",
+        "+       +       + +    + +     +   + + + + +    + +",
+        "+  ++++++ ++++  ++++ + ++ ++ + + + + +++ + +++ + ++",
+        "+  +  +   +   + +  +  +   +     + +   +   +     + +",
+        "+  +  +++  + +   ++     + + + + + +++    +++ +  + +",
+        "+         + + + +   ++ +    +  ++  +++++        + +",
+        "+  +++++++++ ++  + +++++ ++    + ++++ ++++ ++++++++",
+        "+  +   +    +             + +  +                  +",
+        "+++++++++++++++++++++++++++++++++++++++++++++++++++",
+        ]
+
+]
+
+
+def setup_maze(grid):
+    global start_x, start_y, end_x, end_y
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            character = grid[y][x]
+            screen_x = -600 + (x * 24)
+            screen_y = 288 - (y * 24)
+
+            if character == "+":
+                maze.goto(screen_x, screen_y)
+                maze.stamp()
+                walls.append((screen_x, screen_y))
+
+            if character == " " or character == "e":
+                path.append((screen_x, screen_y))
+
+            if character == "e":
+                green.goto(screen_x, screen_y)
+                green.stamp()
+                end_x, end_y = screen_x, screen_y
+
+            if character == "s":
+                start_x, start_y = screen_x, screen_y
+                yellow.goto(screen_x, screen_y)
+
+
+def endProgram():
+    wn.exitonclick()
+    sys.exit()
+
+
+def free():
+    wn.bye()
+
+
+def search(x, y):
+    frontier.append((x, y))
+    solution[x, y] = x, y
+
+    while len(frontier) > 0:  # exit while loop when frontier queue equals zero
+        try:
+            time.sleep(0.02)
+            x, y = frontier.popleft()  # pop next entry in the frontier queue an assign to x and y location
+
+            if (x - 24, y) in path and (x - 24, y) not in visited:  # check the cell on the left
+                cell = (x - 24, y)
+                solution[cell] = x, y  # backtracking routine [cell] is the previous cell. x, y is the current cell
+                # blue.goto(cell)        # identify frontier cells
+                # blue.stamp()
+                frontier.append(cell)  # add cell to frontier list
+                visited.add((x - 24, y))  # add cell to visited list
+
+            if (x, y - 24) in path and (x, y - 24) not in visited:  # check the cell down
+                cell = (x, y - 24)
+                solution[cell] = x, y
+                # blue.goto(cell)
+                # blue.stamp()
+                frontier.append(cell)
+                visited.add((x, y - 24))
+
+            if (x + 24, y) in path and (x + 24, y) not in visited:  # check the cell on the  right
+                cell = (x + 24, y)
+                solution[cell] = x, y
+                # blue.goto(cell)
+                # blue.stamp()
+                frontier.append(cell)
+                visited.add((x + 24, y))
+
+            if (x, y + 24) in path and (x, y + 24) not in visited:  # check the cell up
+                cell = (x, y + 24)
+                solution[cell] = x, y
+                # blue.goto(cell)
+                # blue.stamp()
+                frontier.append(cell)
+                visited.add((x, y + 24))
+            green.goto(x, y)
+            green.stamp()
+        except:
+            messagebox.showerror("Error", "Path couldn't ne searched!!")
+
+
+def backTrack(x, y):
+    try:
+        white.goto(x, y)
+        white.stamp()
+        while (x, y) != (start_x, start_y):  # stop loop when current cells == start cell
+            time.sleep(0.02)
+            white.goto(solution[x, y])  # move the yellow sprite to the key value of solution ()
+            white.stamp()
+            x, y = solution[x, y]  # "key value" now becomes the new key
+        messagebox.showinfo("Success", "Congratulations! Path is Successfully found!!")
+    except:
+        messagebox.showerror("Error", "Sorry!! Path not found!!")
+
+
+def setup_screen():
+    global wn
+    wn = turtle.Screen()
+    wn.bgcolor("black")
+    wn.title("Maze Solving")
+    wn.setup(1300, 700)
+
+
+def start_solving(value):
+    setup_screen()
+    global maze, yellow, blue, green, white, walls, path, visited, frontier, solution
+    maze = Maze()
+    yellow = Yellow()
+    blue = Blue()
+    green = Green()
+    white = White()
+
+    walls = []
+    path = []
+    visited = set()
+    frontier = deque()
+    solution = {}
+
+    setup_maze(grids[value])
+    search(start_x, start_y)
+    backTrack(end_x, end_y)
+    endProgram()
+
